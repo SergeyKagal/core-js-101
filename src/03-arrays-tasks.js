0e2fb7d093140b70ee57d343efc493d521181836
SergeyKagal <3461995@gmail.com>
2022-01-11 06:23:17 +0300

    61 passing

--- src/03-arrays-tasks.js
+++ src/03-arrays-tasks.js
@@ -571,8 +571,8 @@ function group(array, f1, f2) {
  *   [[1, 2], [3, 4], [5, 6]], (x) => x     =>   [ 1, 2, 3, 4, 5, 6 ]
  *   ['one','two','three'], x=>x.split('')  =>   ['o','n','e','t','w','o','t','h','r','e','e']
  */
-function selectMany(/* arr, childrenSelector */) {
-  throw new Error('Not implemented');
+function selectMany(arr, childrenSelector) {
+  return arr.flatMap((x) => childrenSelector(x));
 }
 
 /**
@@ -587,8 +587,12 @@ function selectMany(/* arr, childrenSelector */) {
  *   ['one','two','three'], [2]       => 'three'  (arr[2])
  *   [[[ 1, 2, 3]]], [ 0, 0, 1 ]      => 2        (arr[0][0][1])
  */
-function getElementByIndexes(/* arr, indexes */) {
-  throw new Error('Not implemented');
+function getElementByIndexes(arr, arrInd) {
+  const s = arrInd.length;
+  if (s > 1) {
+    return getElementByIndexes(arr[arrInd.shift()], arrInd);
+  }
+  return arr[arrInd[0]];
 }
 
 /**
@@ -609,8 +613,14 @@ function getElementByIndexes(/* arr, indexes */) {
  *   [ 1, 2, 3, 4, 5, 6, 7, 8 ]   =>  [ 5, 6, 7, 8, 1, 2, 3, 4 ]
  *
  */
-function swapHeadAndTail(/* arr */) {
-  throw new Error('Not implemented');
+function swapHeadAndTail(arr) {
+  if (arr.length % 2 === 0) {
+    const arr1 = arr.splice(0, arr.length / 2);
+    return arr.concat(arr1);
+  }
+  const arr1 = arr.splice(0, arr.length / 2);
+  const mid = arr.splice(0, 1);
+  return arr.concat(mid).concat(arr1);
 }
 
 module.exports = {
